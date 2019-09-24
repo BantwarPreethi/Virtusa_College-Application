9840de4e8a57cfb36772c7f130845d534ea6bba1
bantwarpreethi <preethibantwar15@gmail.com>
2019-09-24 14:38:17 +0530

    Skeleton

---
+++ CollegeApplication/src/com/virtusa/client/UserClient.java
@@ -0,0 +1,14 @@
+package com.virtusa.client;
+
+public class UserClient {
+
+	public UserClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
