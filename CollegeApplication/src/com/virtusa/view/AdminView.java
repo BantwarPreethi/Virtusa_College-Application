a65518a4f76fa7145398e2424332f6514b189073
venka <venka@LAPTOP-A6JAJ39S>
2019-09-25 23:45:53 +0530

    Skeleton and admin module

---
+++ CollegeApplication/src/com/virtusa/view/AdminView.java
@@ -0,0 +1,67 @@
+package com.virtusa.view;
+
+import java.util.Scanner;
+
+public class AdminView 
+{
+	public void adminView()
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Admin LogIn is Successful");
+		System.out.println("1.Approve Applicant");
+		System.out.println("2.Manage Students");
+		System.out.println("3.Manage Faculty");
+		System.out.println("4.Manage Events");
+		System.out.println("5.Manage Course");
+		System.out.println("6.Manage Placements");
+		System.out.println("7.View Students");
+		System.out.println("8.View Faculty");
+		System.out.println("9.LogOut");
+		System.out.println("Enter you choice");
+		int choice= scan.nextInt();
+		
+		switch(choice)
+		{
+		
+		case 1 : 
+				 	break;
+				
+		case 2 :
+					break;
+		
+		case 3 : 
+					break;
+			
+		case 4 :
+					break;
+			
+		case 5 : 
+					break;
+		
+		case 6 :
+					break;
+			
+		case 7 : 
+					break;
+	
+		case 8 :
+					break;
+					
+		case 9 :
+					break;
+			
+			
+			
+			
+			
+			
+		
+		
+		}
+		
+		
+		
+		
+	}
+
+}
