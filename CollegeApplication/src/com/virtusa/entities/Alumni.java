f4f6a018cc497b5103c46ae1665e2982898a3abb
admin <admin@1A-1>
2019-09-26 09:19:54 -0700

    Alumni class

---
+++ CollegeApplication/src/com/virtusa/entities/Alumni.java
@@ -0,0 +1,17 @@
+package com.virtusa.entities;
+
+public class Alumni
+{
+	private int alumniId;
+
+	public int getAlumniId() 
+	{
+		return alumniId;
+	}
+
+	public void setAlumniId(int alumniId) 
+	{
+		this.alumniId = alumniId;
+	}
+
+}
