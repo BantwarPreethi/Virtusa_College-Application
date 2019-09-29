e4c61df1343d2ce068df5aaa18b8ca152939d322
User <User@192.168.0.4>
2019-09-29 22:49:36 +0530

    student

---
+++ CollegeApplication/src/com/virtusa/helper/FactoryStudentDAO.java
@@ -0,0 +1,16 @@
+package com.virtusa.helper;
+
+import com.virtusa.dao.StudentDAO;
+import com.virtusa.service.StudentService;
+import com.virtusa.service.StudentServiceImpl;
+
+public class FactoryStudentDAO {
+
+	public static StudentService createStudentDAO() {
+	StudentService studentService=new StudentServiceImpl();
+		return studentService;
+	}
+
+	
+
+}
---
+++ CollegeApplication/src/com/virtusa/model/StudentModel1.java
@@ -0,0 +1,15 @@
+package com.virtusa.model;
+
+public class StudentModel1 {
+	private int studentId;
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	
+
+}
